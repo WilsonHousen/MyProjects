674db78127e78f8ac16ba20982556c217e10998f
Wilson Housen <wihousen@vassar.edu>
2016-08-30 15:51:15 -0400

    First snapshot

---
+++ Lab1JavaProg/JavaApplication4/src/Cs203Lab1/Cs203Lab1.java
@@ -0,0 +1,17 @@
+
+package Cs203Lab1;
+
+/**
+ *
+ * @author wihousen
+ */
+public class Cs203Lab1 {
+
+    
+    public static void main(String[] args) {
+     
+        System.out.println ("What I wanted to print out");
+        
+    }
+    
+}
